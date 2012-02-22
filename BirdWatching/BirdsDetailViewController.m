8c8c1ade4e20e614dde0ff9b56c48c30149bd448
Bati Zhao <zhaobati@gmail.com>
2012-02-22 14:49:34 +0800

    Displaying Information in the Detail Scene.

--- BirdWatching/BirdsDetailViewController.m
+++ BirdWatching/BirdsDetailViewController.m
@@ -50,4 +50,17 @@
     [super viewDidUnload];
 }
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
 @end
